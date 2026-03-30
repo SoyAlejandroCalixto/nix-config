f2c02dd0b4704d7bb9219e723a4f4b43591dafff
soyalejandrocalixto <soyalejandrocalixto@gmail.com>
2026-03-30 21:26:57 +0100

    padding wezterm cambiadao

--- conf/shell.nix
+++ conf/shell.nix
@@ -67,7 +67,7 @@
         window_close_confirmation = "NeverPrompt",
         use_fancy_tab_bar = false,
         hide_tab_bar_if_only_one_tab = true,
-        window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+        window_padding = { left = 8, right = 8, top = 8, bottom = 8 }
       }
     '';
   };
